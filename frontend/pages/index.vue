08f7414faf4ebc8c63dfd2ac7ec1d90bd302544d
sasaki <skyo.97.admj@gmail.com>
2021-07-02 15:07:22 +0900

    【Test】Sending a message

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -3,6 +3,7 @@
     <h1 class="title">
       {{ text }}
     </h1>
+    <button @click="testButton()">ボタン</button>
   </div>
 </template>
 
@@ -23,6 +24,21 @@ export default {
     window.liff.init({
       liffId: process.env.LIFF_ID,
     });
+  },
+  methods: {
+    testButton() {
+      if (!liff.isInClient()) {
+        console.log('外部ブラウザまたはLINE内ブラウザで動作させている');
+      } else {
+        console.log('LIFFブラウザで動作させている');
+        liff.sendMessages([
+          {
+            type: 'text',
+            text: 'Hello World!',
+          },
+        ]);
+      }
+    },
   },
 };
 </script>
