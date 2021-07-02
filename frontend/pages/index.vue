f60b264e0610eb40bfdbd3ff1e9bf85ba2d9f5ff
sasaki <skyo.97.admj@gmail.com>
2021-07-02 16:53:21 +0900

    Creating a form

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,20 +2,10 @@
   <div>
     <!-- 姓 -->
     <FormLabel :title="'姓'" />
-    <FormInput
-      :type="'firstName'"
-      :placeholder="'姓'"
-      :value="firstName"
-      @input="firstName = $event"
-    />
+    <FormInput :type="'text'" :placeholder="'姓'" :value="firstName" @input="firstName = $event" />
     <!-- 名 -->
     <FormLabel :title="'名'" />
-    <FormInput
-      :type="'lastName'"
-      :placeholder="'名'"
-      :value="lastName"
-      @input="lastName = $event"
-    />
+    <FormInput :type="'text'" :placeholder="'名'" :value="lastName" @input="lastName = $event" />
     <!-- メールアドレス -->
     <FormLabel :title="'メールアドレス'" />
     <FormInput
@@ -61,7 +51,7 @@ export default {
         liff.sendMessages([
           {
             type: 'text',
-            text: this.email,
+            text: `姓：${this.firstName}\n 名：${this.lastName}\n メールアドレス：${this.email}`,
           },
         ]);
         // アプリを閉じる
