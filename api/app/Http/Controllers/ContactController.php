5c06bef4ead16bb42a34d48aedb6d76c0da1a871
sasaki <skyo.97.admj@gmail.com>
2021-07-04 08:48:44 +0900

    test

--- api/app/Http/Controllers/ContactController.php
+++ api/app/Http/Controllers/ContactController.php
@@ -14,7 +14,7 @@ class ContactController extends Controller
 {
   public function index()
   {
-    return Contact::all();
+    return "Hello World!"
   }
 
   public function store(StoreContact $request)
