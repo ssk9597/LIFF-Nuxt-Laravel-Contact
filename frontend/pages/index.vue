be803a6929c76b1cbb6fb42c5a32351ccfce6bab
sasaki <skyo.97.admj@gmail.com>
2021-07-04 10:12:55 +0900

    test

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,18 +1,33 @@
 <template>
   <div>
     <Form />
-    <ContactDetail />
+    <!-- <ContactDetail /> -->
+    <div class="card" v-for="(contact, index) in contacts" :key="index">
+      <h3 class="contact-content">{{ contact.contact }}</h3>
+      <p class="contact-name">{{ contact.name }}</p>
+      <p class="contact-email">{{ contact.email }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 import Form from '@/components/Template/Form.vue';
-import ContactDetail from '@/components/Atoms/ContactDetail.vue';
+// import ContactDetail from '@/components/Atoms/ContactDetail.vue';
 
 export default {
   components: {
     Form,
-    ContactDetail,
+    // ContactDetail,
+  },
+  async asyncData({ $axios }) {
+    const contacts = await $axios.$get('/contacts/index');
+    console.log(contacts);
+    return { contacts };
+  },
+  mounted() {
+    window.liff.init({
+      liffId: process.env.LIFF_ID,
+    });
   },
 };
 </script>
