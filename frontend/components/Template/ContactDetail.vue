c5c2008b183dfdec6c722a0867af26bb9d15caa0
sasaki <skyo.97.admj@gmail.com>
2021-07-04 10:42:11 +0900

    Test

--- frontend/components/Template/ContactDetail.vue
+++ frontend/components/Template/ContactDetail.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h2 class="heading">お問い合わせ内容</h2>
     <div class="card" v-for="(contact, index) in contacts" :key="index">
       <h3 class="contact-content">{{ contact.contact }}</h3>
       <p class="contact-name">{{ contact.name }}</p>
@@ -24,15 +25,33 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.heading {
+  padding-top: 50px;
+  padding-bottom: 0.35em;
+  font-size: 1.5em;
+  color: #333;
+  border-bottom: 3px solid #ccc;
+  max-width: 400px;
+  width: 100%;
+  margin: 0 auto 30px auto;
+  text-align: center;
+}
 .card {
   box-sizing: border-box;
-  width: 15em;
+  max-width: 300px;
+  width: 100%;
   padding: 1.2em 0.8em;
   margin: 10px auto;
   color: #333;
   border: 0.5px solid #e9eaea;
   border-radius: 3px;
   transition: 0.3s;
+}
+
+.contact-content {
+  font-size: 1.25rem;
+  padding-bottom: 10px;
+  font-weight: bold;
 }
 
 .content-name,
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,80 +1,21 @@
 <template>
   <div>
     <Form />
-    <!-- <ContactDetail /> -->
-    <h2 class="heading">お問い合わせ内容</h2>
-    <div class="card" v-for="(contact, index) in contacts" :key="index">
-      <h3 class="contact-content">{{ contact.contact }}</h3>
-      <p class="contact-name">{{ contact.name }}</p>
-      <p class="contact-email">{{ contact.email }}</p>
-    </div>
+    <ContactDetail />
   </div>
 </template>
 
 <script>
 import Form from '@/components/Template/Form.vue';
-// import ContactDetail from '@/components/Atoms/ContactDetail.vue';
+import ContactDetail from '@/components/Template/ContactDetail.vue';
 
 export default {
   components: {
     Form,
-    // ContactDetail,
-  },
-  async asyncData({ $axios }) {
-    const contacts = await $axios.$get('/contacts/index');
-    console.log(contacts);
-    return { contacts };
-  },
-  mounted() {
-    window.liff.init({
-      liffId: process.env.LIFF_ID,
-    });
+    ContactDetail,
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.heading {
-  padding-top: 50px;
-  padding-bottom: 0.35em;
-  font-size: 1.5em;
-  color: #333;
-  border-bottom: 3px solid #ccc;
-  max-width: 400px;
-  width: 100%;
-  margin: 0 auto 30px auto;
-  text-align: center;
-}
-.card {
-  box-sizing: border-box;
-  max-width: 300px;
-  width: 100%;
-  padding: 1.2em 0.8em;
-  margin: 10px auto;
-  color: #333;
-  border: 0.5px solid #e9eaea;
-  border-radius: 3px;
-  transition: 0.3s;
-}
-
-.contact-content {
-  font-size: 1.25rem;
-  padding-bottom: 10px;
-  font-weight: bold;
-}
-
-.content-name,
-.contact-email {
-  padding: 0.8em 0;
-  font-size: 0.8em;
-}
-
-.contact-email {
-  color: #00b5ad;
-  cursor: pointer;
-
-  &:hover {
-    border-bottom: #00b5ad 1px solid;
-  }
-}
 </style>
