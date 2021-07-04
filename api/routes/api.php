30aaeefea81ff3aea0942568a859c4cc066a78d8
sasaki <skyo.97.admj@gmail.com>
2021-07-04 09:45:28 +0900

    Update Routing

--- api/routes/api.php
+++ api/routes/api.php
@@ -7,12 +7,6 @@ use Illuminate\Http\Request;
 | API Routes
 |--------------------------------------------------------------------------
 */
-
-Route::get("/", function () {
-  return "Hello World!";
-});
-
 // contacts
 Route::get("/contacts/index", "ContactController@index");
 Route::post("/contacts", "ContactController@store");
-// Route::resource('contacts', "ContactController", ["only" => ["index", "store"]]);
