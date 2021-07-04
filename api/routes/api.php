64a4810e6294e9b4e4a0c892b54e174986d136b6
sasaki <skyo.97.admj@gmail.com>
2021-07-04 09:00:27 +0900

    test

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ Route::get("/", function () {
 });
 
 // contacts
-Route::get("/contacts/index", "ContactController@index");
+// Route::get("/contacts/index", "ContactController@index");
 Route::post("/contacts", "ContactController@store");
+Route::get("/contacts/index", function () {
+  Log::info("OK");
+  return "Hello World!";
+});
 // Route::resource('contacts', "ContactController", ["only" => ["index", "store"]]);
